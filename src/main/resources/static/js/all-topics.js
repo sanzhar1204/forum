a97fecdc6296cf9acc525b6cb3e3b3937c07c037
Жумабек Санжар <zsanzhar@gmail.com>
2022-01-29 18:46:59 +0600

    Add all topics view

---
+++ src/main/resources/static/js/all-topics.js
@@ -0,0 +1,33 @@
+"use strict";
+
+refreshAllTopics();
+
+function refreshAllTopics(){
+    $('#pagination').pagination({
+        dataSource: `/api/topics?sort=createdAt,desc`,
+        locator: 'content',
+        totalNumberLocator: function(response) {
+            return response.totalElements;
+        },
+        alias: {
+            pageNumber: 'page',
+            pageSize: 'size',
+        },
+        className: 'paginationjs-big',
+        callback: function(data, pagination) {
+            refreshAllElements(data);
+        }
+    })
+}
+function refreshAllElements(topics){
+    let topicList = $('#topicList');
+    topicList.empty();
+    for (var i = 0; i < topics.length; i++) {
+        topicList.append(createTopicElement(topics[i]));
+    }
+}
+
+function createTopicElement(topincInfo){
+    let topicElement = $.parseHTML(`<tr><th scope="row"><a href="/topics/${topincInfo.topic.id}">${topincInfo.topic.id}</a></th><td><a href="/topics/${topincInfo.topic.id}">${topincInfo.topic.name}</td></a><td><a href="/topics/${topincInfo.topic.id}">${topincInfo.topic.createdBy.email}</a></td><td><a href="/topics/${topincInfo.topic.id}">${topincInfo.topic.createdAt}</a></td><td><a href="/topics/${topincInfo.topic.id}">${topincInfo.commentsCount}</a></td></tr>`);
+    return topicElement;
+}
